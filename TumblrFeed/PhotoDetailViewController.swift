f0e1e226bc0a4eff4fb4c47c6cfd89f5e219eb6a
Sylvia Mach <sylviamach@Sylvias-MacBook-Pro.local>
2017-02-09 20:15:56 -0800

    add image view

---
+++ TumblrFeed/PhotoDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoDetailViewController.swift
+//  TumblrFeed
+//
+//  Created by Sylvia Mach on 2/9/17.
+//  Copyright © 2017 Sylvia Mach. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    @IBOutlet var photoView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
