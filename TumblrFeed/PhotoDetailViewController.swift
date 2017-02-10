43613721168dae96d817a821f8295f0a1c8e8f6a
dreamsicl <vphong@ucsd.edu>
2017-02-09 22:02:54 -0800

    required cases

--- TumblrFeed/PhotoDetailViewController.swift
+++ TumblrFeed/PhotoDetailViewController.swift
@@ -11,8 +11,26 @@ import UIKit
 class PhotoDetailViewController: UIViewController {
 
     @IBOutlet var photoView: UIImageView!
+    
+    var post: NSDictionary!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let photos = post.value(forKeyPath: "photos") as? [NSDictionary] {
+            let imageUrlString = photos[0].value(forKeyPath: "original_size.url")
+            //print(imageUrlString)
+            if let imageUrl = URL(string: imageUrlString! as! String){
+                //let imageUrl = URL(fileURLWithPath: imageUrlString as! String)
+                photoView.setImageWith(imageUrl)
+                
+            } else {
+                
+            }
+        } else {
+            
+        }
+        
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +39,7 @@ class PhotoDetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 
     /*
     // MARK: - Navigation
--- TumblrFeed/PhotosViewController.swift
+++ TumblrFeed/PhotosViewController.swift
@@ -9,20 +9,29 @@
 import UIKit
 import AFNetworking
 
-class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     var posts: [NSDictionary] = []
-    
+    var isMoreDataLoading = false
+    var offset: Int = 0
+    var limit: Int = 3
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = 248
-        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
 
         // Do any additional setup after loading the view.
-        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        requestNetwork(offset: 0)
+    }
+    
+    func requestNetwork(offset: Int){
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV&offset=\(offset)&limit=\(limit)")
         let request = URLRequest(url: url!)
         let session = URLSession(
             configuration: URLSessionConfiguration.default,
@@ -51,13 +60,72 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
         });
         task.resume()
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading){
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffSetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            if (scrollView.contentOffset.y > scrollOffSetThreshold && tableView.isDragging){
+                isMoreDataLoading = true
+                self.offset += self.limit
+                loadMoreData(offset: self.offset)
+                
+            }
+        }
+    }
+    
+    func loadMoreData(offset: Int){
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV&offset=\(offset)&limit=\(limit)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                self.isMoreDataLoading = false
+                
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        self.posts += (responseFieldDictionary["posts"] as! [NSDictionary])
+                        print("loadMoreData was called")
+                        
+                        self.tableView.reloadData()
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                    }
+                }
+                
+        });
+        task.resume()
+        
+    }
 
+    func refreshControlAction(_ refreshControl: UIRefreshControl){
+        
+        requestNetwork(offset: 0)
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        print("posts.count = \(posts.count)")
        return posts.count
         
     }
@@ -86,14 +154,29 @@ class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewD
         
         return cell
     }
-    /*
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let destination = segue.destination as! PhotoDetailViewController
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        
+        
+        let post = posts[(indexPath?.row)!]
+        
+        destination.post = post
+        
+        
     }
-    */
 
 }
